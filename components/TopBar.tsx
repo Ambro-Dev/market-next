57cdd9a05610b9d1e3b61cc1a6b96039c7660812
Ambro-Dev <piotr.ambroziak@ambro.dev>
2023-08-18 22:38:43 +0200

    image next

--- components/TopBar.tsx
+++ components/TopBar.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import Image from "next/image";
-import logo from "../public/gielda-fenilo.webp";
+import logo from "../public/logo.png";
 import {
   NavigationMenuContent,
   NavigationMenuTrigger,
